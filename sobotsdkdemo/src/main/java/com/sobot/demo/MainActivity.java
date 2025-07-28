376c274764b622e11a4fe0882979b109266c3604
zhengnw@sobot.com <znw123456>
2025-07-28 14:47:23 +0800

    4.2.7

--- sobotsdkdemo/src/main/java/com/sobot/demo/MainActivity.java
+++ sobotsdkdemo/src/main/java/com/sobot/demo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sobot.demo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,13 +10,21 @@ import com.sobot.chat.ZCSobotApi;
 import com.sobot.chat.api.model.Information;
 
 public class MainActivity extends AppCompatActivity {
+    private Button qidong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Information info=new Information();
-        info.setApp_key("1c1da2c0aad047d7ba1d14ecd18ae4f6");
-        ZCSobotApi.openZCChat(MainActivity.this,info);
+        qidong=findViewById(R.id.qidong);
+        qidong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Information info=new Information();
+                info.setApp_key("1c1da2c0aad047d7ba1d14ecd18ae4f6");
+                ZCSobotApi.openZCChat(MainActivity.this,info);
+            }
+        });
+
     }
 }
